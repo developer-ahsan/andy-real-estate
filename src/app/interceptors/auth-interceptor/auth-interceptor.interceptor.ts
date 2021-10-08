06b08eb09604eb504f9ee1c845288850574af40c
CINNOVA\hrakshani <hrakshani@cinnova.com>
2021-10-08 22:01:35 +0500

    http interceptor added

---
+++ src/app/interceptors/auth-interceptor/auth-interceptor.interceptor.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    token: any;
+    constructor() {
+    }
+
+    intercept(
+        request: HttpRequest<unknown>,
+        next: HttpHandler
+    ): Observable<HttpEvent<unknown>> {
+        let url = null;
+        this.token = localStorage.getItem('token');
+        console.log('request', request);
+        if (!request.url.includes("dashboard")) {
+             url = `http://localhost:5000/dev/${request.url}`
+            //url = `https://cqlfqg2orc.execute-api.us-east-2.amazonaws.com/${request.url}`
+        }
+
+
+
+        request = request.clone({
+
+            url: url ? url : request.url,
+            setHeaders: {
+                Authorization: this.token,
+            },
+        });
+        return next.handle(request);
+    }
+}
