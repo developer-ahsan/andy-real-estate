95c1669134eb925de654e9b7a1306b08a795e164
Ehsan <eullah@cinnova.com>
2022-01-25 12:36:05 -0800

    environment prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     signInAuth: "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDdIDmQFMIa3aXSnoudkf3At_zQ9faFs2k",
     customerList: "https://consolidus-staging.azurewebsites.net/api/users",
     customer: "https://consolidus-staging.azurewebsites.net/api/user",
-    orders: "https://consolidus-staging.azurewebsites.net/api/orders"
+    orders: "https://consolidus-staging.azurewebsites.net/api/orders",
+    stores: "https://consolidus-staging.azurewebsites.net/api/stores"
 };
