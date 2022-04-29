19aec12516d627a4225dab1161e85e53852fee5a
Ehsan <eullah@cinnova.com>
2022-04-29 14:07:29 +0500

    store settings form update

--- src/app/modules/admin/apps/file-manager/navigation/store-settings/store-settings.component.ts
+++ src/app/modules/admin/apps/file-manager/navigation/store-settings/store-settings.component.ts
@@ -9,7 +9,18 @@ export class StoreSettingsComponent implements OnInit {
   @Input() selectedStore: any;
   @Input() isLoading: boolean;
   @Output() isLoadingChange = new EventEmitter<boolean>();
+  selected = 'YES';
   private _unsubscribeAll: Subject<any> = new Subject<any>();
+  quillModules: any = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'color': [] }, { 'background': ['white'] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean']
+    ]
+  };
 
   constructor() { }
 
