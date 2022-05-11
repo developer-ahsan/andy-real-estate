16a8de2b9a6c7b4e7298c580cf3ef8e9e90512c9
Ehsan <eullah@cinnova.com>
2022-05-11 14:28:01 +0500

    product suppliers

--- src/app/modules/admin/apps/file-manager/navigation/products-suppliers/products-suppliers.component.ts
+++ src/app/modules/admin/apps/file-manager/navigation/products-suppliers/products-suppliers.component.ts
@@ -3,58 +3,24 @@ import { Subject } from 'rxjs';
 import { FileManagerService } from 'app/modules/admin/apps/file-manager/file-manager.service';
 import { takeUntil } from 'rxjs/operators';
 
-export interface PeriodicElement {
-  spid: number;
-  name: string;
-  vendor: string;
-  master: string;
-  store: string;
-  desc: boolean;
-  image: boolean;
-  video: boolean;
-  colors: boolean;
-  techno_logo: boolean;
-  misc: boolean
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { spid: 1, name: '15 Adult Flip Flops', vendor: 'Cathy', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 2, name: '3080 Snowflake Ice Scraper', vendor: 'Saul', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 3, name: 'Lithium', vendor: 'GaryLine LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 4, name: 'Beryllium', vendor: 'SanMar', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 5, name: 'Boron', vendor: 'A-One', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 6, name: 'Carbon', vendor: 'Delta Apparel LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 1, name: '15 Adult Flip Flops', vendor: 'Cathy', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 2, name: '3080 Snowflake Ice Scraper', vendor: 'Saul', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 3, name: 'Lithium', vendor: 'GaryLine LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 4, name: 'Beryllium', vendor: 'SanMar', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 5, name: 'Boron', vendor: 'A-One', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 6, name: 'Carbon', vendor: 'Delta Apparel LLC', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 1, name: '15 Adult Flip Flops', vendor: 'Cathy', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 2, name: '3080 Snowflake Ice Scraper', vendor: 'Saul', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 3, name: 'Lithium', vendor: 'GaryLine LLC', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 4, name: 'Beryllium', vendor: 'SanMar', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 5, name: 'Boron', vendor: 'A-One', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 6, name: 'Carbon', vendor: 'Delta Apparel LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 1, name: '15 Adult Flip Flops', vendor: 'Cathy', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 2, name: '3080 Snowflake Ice Scraper', vendor: 'Saul', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 3, name: 'Lithium', vendor: 'GaryLine LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 4, name: 'Beryllium', vendor: 'SanMar', master: 'Active', store: 'Online', desc: true, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 5, name: 'Boron', vendor: 'A-One', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false },
-  { spid: 6, name: 'Carbon', vendor: 'Delta Apparel LLC', master: 'Active', store: 'Online', desc: false, image: false, video: false, colors: false, techno_logo: false, misc: false }
-];
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 
 @Component({
   selector: 'app-products-suppliers',
   templateUrl: './products-suppliers.component.html'
 })
+
 export class ProductsSuppliersComponent implements OnInit {
   @Input() selectedStore: any;
   @Input() isLoading: boolean;
   @Output() isLoadingChange = new EventEmitter<boolean>();
   private _unsubscribeAll: Subject<any> = new Subject<any>();
-  displayedColumns: string[] = ['spid', 'name', 'vendor', 'master', 'store', 'desc', 'image', 'video', 'colors', 'techno_logo', 'misc'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['pid', 'spid', 'sid', 'name', 'master', 'store'];
+  dataSource = [];
+  dataSourceTotalRecord: number;
+  dataSourceLoading = false;
   page: number = 1;
 
   constructor(
@@ -63,6 +29,7 @@ export class ProductsSuppliersComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.dataSourceLoading = true;
     this.getMainStoreCall(this.page);
     this.isLoadingChange.emit(false);
   }
@@ -70,14 +37,29 @@ export class ProductsSuppliersComponent implements OnInit {
   getMainStoreCall(page) {
     const { pk_storeID } = this.selectedStore;
 
-    // Get the offline products
-    this._fileManagerService.getStoreCategory(pk_storeID, page)
+    // Get the supplier products
+    this._fileManagerService.getProducts(pk_storeID, page)
       .pipe(takeUntil(this._unsubscribeAll))
       .subscribe((response: any) => {
-        console.log("response", response)
+        this.dataSource = response["data"];
+        this.dataSourceTotalRecord = response["totalRecords"];
+        this.dataSourceLoading = false;
 
         // Mark for check
         this._changeDetectorRef.markForCheck();
       });
-  }
+  };
+
+  getNextData(event) {
+    const { previousPageIndex, pageIndex } = event;
+
+    if (pageIndex > previousPageIndex) {
+      this.page++;
+    } else {
+      this.page--;
+    };
+    this.getMainStoreCall(this.page);
+  };
+
 }
+
