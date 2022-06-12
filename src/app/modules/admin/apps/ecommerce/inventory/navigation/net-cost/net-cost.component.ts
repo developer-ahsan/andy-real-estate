0e39248b0b452741a134e4c6b1e8b1e851ae14f5
Ehsan Ullah Soomro <eullah@cinnova.com>
2022-06-13 03:04:40 +0500

    few updates

--- src/app/modules/admin/apps/ecommerce/inventory/navigation/net-cost/net-cost.component.ts
+++ src/app/modules/admin/apps/ecommerce/inventory/navigation/net-cost/net-cost.component.ts
@@ -135,7 +135,6 @@ export class NetCostComponent implements OnInit {
       msrp: [''],
       internalComments: ['']
     });
-    // this.netCostForm.patchValue(customer);
 
     this._inventoryService.getCoOp(fk_supplierID)
       .pipe(takeUntil(this._unsubscribeAll))
@@ -235,26 +234,138 @@ export class NetCostComponent implements OnInit {
       .pipe(takeUntil(this._unsubscribeAll))
       .subscribe((productPricing) => {
         if (productPricing["data"]["success"]) {
-          if (productPricing["data"]["result"]["Envelope"]["Body"]["GetConfigurationAndPricingResponse"]["ErrorMessage"]["description"] != "Data not found") {
-            const data = productPricing["data"]["result"]["Envelope"]["Body"]["GetConfigurationAndPricingResponse"]["Configuration"]["PartArray"];
+          if (productPricing["data"]["result"]["Envelope"]["Body"]["GetConfigurationAndPricingResponse"]?.ErrorMessage?.description == "Data not found") {
+            this._snackBar.open("No data found against this product number", '', {
+              horizontalPosition: 'center',
+              verticalPosition: 'bottom',
+              duration: 3500
+            });
+            this.netCostFetchLoader = false;
+
+            // Mark for check
+            this._changeDetectorRef.markForCheck();
+          } else {
+            const pricingDataArray = productPricing["data"]["result"]["Envelope"]["Body"]["GetConfigurationAndPricingResponse"]["Configuration"]["PartArray"];
+            const { blnApparel } = this.selectedProduct;
+            let obj = {};
+
+            if (blnApparel) {
+              // Update Standard Cost
+              if (pricingDataArray?.length) {
+                for (let i = 0; i <= 5; i++) {
+                  const { price } = pricingDataArray[i]["PartPriceArray"][0];
+                  if (i == 0) {
+                    obj["standardCostOne"] = price
+                  }
+
+                  if (i == 1) {
+                    obj["standardCostTwo"] = price
+                  }
+
+                  if (i == 2) {
+                    obj["standardCostThree"] = price
+                  }
+
+                  if (i == 3) {
+                    obj["standardCostFour"] = price
+                  }
+
+                  if (i == 4) {
+                    obj["standardCostFive"] = price
+                  }
+
+                  if (i == 5) {
+                    obj["standardCostSix"] = price
+                  }
+                }
+              } else {
+                obj = {
+                  standardCostOne: null,
+                  standardCostTwo: null,
+                  standardCostThree: null,
+                  standardCostFour: null,
+                  standardCostFive: null,
+                  standardCostSix: null
+                };
+              }
+            } else {
+              // Updata Quantity and standard cost
+              if (pricingDataArray?.length) {
+                for (let i = 0; i <= 5; i++) {
+                  const { minQuantity, price } = pricingDataArray[i]["PartPriceArray"][0];
+                  if (i == 0) {
+                    obj["quantityOne"] = minQuantity;
+                    obj["standardCostOne"] = price;
+                  }
+
+                  if (i == 1) {
+                    obj["quantityTwo"] = minQuantity;
+                    obj["standardCostTwo"] = price;
+                  }
+
+                  if (i == 2) {
+                    obj["quantityThree"] = minQuantity;
+                    obj["standardCostThree"] = price;
+                  }
+
+                  if (i == 3) {
+                    obj["quantityFour"] = minQuantity;
+                    obj["standardCostFour"] = price;
+                  }
+
+                  if (i == 4) {
+                    obj["quantityFive"] = minQuantity;
+                    obj["standardCostFive"] = price;
+                  }
+
+                  if (i == 5) {
+                    obj["quantitySix"] = minQuantity;
+                    obj["standardCostSix"] = price;
+                  }
+                }
+              } else {
+                obj = {
+                  quantityOne: null,
+                  quantityTwo: null,
+                  quantityThree: null,
+                  quantityFour: null,
+                  quantityFive: null,
+                  quantitySix: null,
+                  standardCostOne: null,
+                  standardCostTwo: null,
+                  standardCostThree: null,
+                  standardCostFour: null,
+                  standardCostFive: null,
+                  standardCostSix: null
+                };
+              }
+            };
+
+            this.netCostForm.patchValue(obj);
+
             this._snackBar.open("Data fetched successfully", '', {
               horizontalPosition: 'center',
               verticalPosition: 'bottom',
               duration: 3500
             });
             this.netCostFetchLoader = false;
+
             // Mark for check
             this._changeDetectorRef.markForCheck();
-          } else {
-            this._snackBar.open("No data found against this product number", '', {
-              horizontalPosition: 'center',
-              verticalPosition: 'bottom',
-              duration: 3500
-            });
           }
 
         }
         this.netCostFetchLoader = false;
+        // Mark for check
+        this._changeDetectorRef.markForCheck();
+      }, err => {
+        this._snackBar.open("Some error occured while fetching promo standards data", '', {
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+          duration: 3500
+        });
+        this.netCostFetchLoader = false;
+
         // Mark for check
         this._changeDetectorRef.markForCheck();
       })
