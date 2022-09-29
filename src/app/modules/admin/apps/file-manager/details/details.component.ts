fbf9cbcc9017d5280425db72877e1324f8d80101
Ahsan Ayoub <aayoub@cinnova.local>
2022-09-29 16:02:55 +0500

    update consolidated bil

--- src/app/modules/admin/apps/file-manager/details/details.component.ts
+++ src/app/modules/admin/apps/file-manager/details/details.component.ts
@@ -38,7 +38,7 @@ export class StoresDetailsComponent implements OnInit, OnDestroy {
   drawerOpened: boolean = true;
 
   // Default details screen
-  selectedIndex: string = "Dashboard";
+  selectedIndex: string = "Consolidated Bill";
 
   /**
    * Constructor
