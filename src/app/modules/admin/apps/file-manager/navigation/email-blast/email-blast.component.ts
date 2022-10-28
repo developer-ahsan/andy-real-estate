70cba99ebdbaaff5276b67b644cca28f7fa38970
Ahsan Ayoub <aayoub@cinnova.com>
2022-10-28 18:33:07 +0500

    email blast

--- src/app/modules/admin/apps/file-manager/navigation/email-blast/email-blast.component.ts
+++ src/app/modules/admin/apps/file-manager/navigation/email-blast/email-blast.component.ts
@@ -1,29 +1,10 @@
-import { Component, Input, Output, OnInit, EventEmitter, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { FileManagerService } from '../../store-manager.service';
 import moment from "moment";
-
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
-  ApexTitleSubtitle,
-  ApexStroke,
-  ApexGrid,
-  ApexFill,
-  ApexMarkers,
-  ApexYAxis
-} from "ng-apexcharts";
-
-interface ChartData {
-  name: string;
-  data: number[];
-};
 
 @Component({
   selector: 'app-email-blast',
@@ -90,85 +71,11 @@ export class EmailBlastComponent implements OnInit, OnDestroy {
   processDataColumns: string[] = ['clicks', 'requests', 'processed', 'delivered'];
   processDate: any;
   subProcessData: any;
-
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<any>;
-  chartSeries: ChartData[] = [];
-
-
   constructor(
     private _fileManagerService: FileManagerService,
     private _changeDetectorRef: ChangeDetectorRef,
     private _snackBar: MatSnackBar,
-  ) {
-    this.chartOptions = {
-      series: [
-        {
-          name: "Likes",
-          data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
-        },
-        {
-          name: "Clicks",
-          data: [1, 3, 2, 9, 9, 9, 13, 9, 12, 7, 58, 7, 17, 9, 17, 2, 7, 5]
-        }
-      ],
-      chart: {
-        height: 500,
-        type: "line"
-      },
-      stroke: {
-        width: 7,
-        curve: "smooth"
-      },
-      xaxis: {
-        type: "datetime",
-        categories: [
-          "1/11/2000",
-          "2/11/2000",
-          "3/11/2000",
-          "4/11/2000",
-          "5/11/2000",
-          "6/11/2000",
-          "7/11/2000",
-          "8/11/2000",
-          "9/11/2000",
-          "10/11/2000",
-          "11/11/2000",
-          "12/11/2000",
-          "1/11/2001",
-          "2/11/2001",
-          "3/11/2001",
-          "4/11/2001",
-          "5/11/2001",
-          "6/11/2001"
-        ]
-      },
-      title: {
-        text: "Statistics",
-        align: "center",
-        style: {
-          fontSize: "20px",
-          color: "#0F172A"
-        }
-      },
-      markers: {
-        size: 4,
-        colors: ["#0F172A"],
-        strokeColors: "#fff",
-        strokeWidth: 2,
-        hover: {
-          size: 7
-        }
-      },
-      yaxis: {
-        min: -10,
-        max: 40,
-        title: {
-          text: "Engagement"
-        }
-      }
-    };
-  }
+  ) { }
 
   ngOnInit(): void {
     this.isLoadingChange.emit(false);
@@ -359,7 +266,6 @@ export class EmailBlastComponent implements OnInit, OnDestroy {
         .subscribe(res => {
           this.processDataLoader = false;
           this.processData = res["data"]
-          console.log(this.processData)
           this._changeDetectorRef.markForCheck();
         }, err => {
           this.processDataLoader = false;
