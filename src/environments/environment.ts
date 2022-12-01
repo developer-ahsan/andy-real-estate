b95df789bfcd233fc1c1d7ae334bf9d6a67ba7b1
Ehsan Ullah Soomro <eullah@cinnova.com>
2022-12-01 13:00:23 +0500

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: false,
   signInAuth:
     "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDdIDmQFMIa3aXSnoudkf3At_zQ9faFs2k",
   mediaKey: "DE3339BDDF8B5D1A6BD3D965D7CCB4B6A7234672505A0023485852397777D05E",
   mediaAccessUrl: "https://admin.consolidus.com/ssadm/imageUpload.cfm",
-  customerList: "http://localhost:7071/api/users",
-  customer: "http://localhost:7071/api/user",
-  orders: "http://localhost:7071/api/orders",
-  stores: "http://localhost:7071/api/stores",
-  storeNewUrl: "http://localhost:7071/api/stores",
+  customerList: "https://consolidus-staging.azurewebsites.net/api/users",
+  customer: "https://consolidus-staging.azurewebsites.net/api/user",
+  orders: "https://consolidus-staging.azurewebsites.net/api/orders",
+  stores: "https://consolidus-staging.azurewebsites.net/api/stores",
+  storeNewUrl: "https://stores-staging.azurewebsites.net/api/stores",
   storeNewUrlRapidBuild: "https://stores-staging.azurewebsites.net/api/rapid_build",
-  products: "http://localhost:7071/api/products",
-  storeProducts: "http://localhost:7100/api/store_products",
-  system: "http://localhost:7071/api/system",
+  products: "https://consolidus-staging.azurewebsites.net/api/products",
+  storeProducts: "https://consolidus-staging.azurewebsites.net/api/store_products",
+  system: "https://consolidus-staging.azurewebsites.net/api/system",
   campaignMedia: "https://assets.consolidus.com/globalAssets/Campaigns",
   productMedia: "https://assets.consolidus.com/globalAssets/Products",
   imprintMedia: "https://assets.consolidus.com/globalAssets/Imprints",
@@ -34,6 +35,8 @@ export const environment = {
     appId: "1:187396735094:web:4127587a84d92215ec1f57",
   },
 };
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -43,4 +46,4 @@ export const environment = {
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 // https://consolidus-staging.azurewebsites.net/api/
-// http://localhost:7100/api/
+// http://localhost:7071/api/
