b88dcd6ae6c4878c126f14acd3fb21628dd02507
Ahsan Ayoub <aayoub@cinnova.com>
2023-03-30 18:37:02 +0500

    rbuild

--- src/app/modules/admin/rapidBuild/components/rapid-build.component.ts
+++ src/app/modules/admin/rapidBuild/components/rapid-build.component.ts
@@ -57,6 +57,7 @@ export class RapidBuildComponent {
      */
 
     ngOnInit(): void {
+        console.log()
         this._router.events.subscribe((event) => {
             if (event instanceof NavigationEnd) {
                 this.selectedScreeen = this.route.children[0].snapshot.data.title;
