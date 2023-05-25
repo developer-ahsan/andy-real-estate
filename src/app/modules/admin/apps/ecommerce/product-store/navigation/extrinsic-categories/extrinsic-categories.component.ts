8d07aaa79e640bcc77903cca84e31a22fa2f1a77
Ahsan Ayoub <aayoub@cinnova.com>
2023-05-25 12:20:15 +0500

    extrinsic categories

--- src/app/modules/admin/apps/ecommerce/product-store/navigation/extrinsic-categories/extrinsic-categories.component.ts
+++ src/app/modules/admin/apps/ecommerce/product-store/navigation/extrinsic-categories/extrinsic-categories.component.ts
@@ -17,6 +17,8 @@ export class ExtrinsicComponent implements OnInit, OnDestroy {
   extrinsicCat: string = '';
   isUpdateLoading: boolean = false;
 
+  extrinsicValues = ['Apparel', 'Awards', 'Bags', 'Display Materials', 'Drinkware', 'Food', 'Fulfillment', 'Giveaways', 'Masks', 'Office Supplies', 'Pins/Name Tags', 'Specialty', 'Technology Items', 'Other'];
+  ngExtrinsic = '';
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
     private _storeService: StoreProductService
@@ -28,24 +30,35 @@ export class ExtrinsicComponent implements OnInit, OnDestroy {
   getStoreProductDetail() {
     this._storeService.product$.pipe(takeUntil(this._unsubscribeAll)).subscribe(res => {
       this.selectedProduct = res["data"][0];
-      this.extrinsicCat = this.selectedProduct.extrinsicCategory;
+      let index = this.extrinsicValues.findIndex(item => item == this.selectedProduct.extrinsicCategory);
+      if (index == -1) {
+        this.ngExtrinsic = 'Other';
+        this.extrinsicCat = this.selectedProduct.extrinsicCategory;
+      } else {
+        this.ngExtrinsic = this.extrinsicValues[index];
+      }
       this.isLoadingChange.emit(false);
       this.isLoading = false;
       this._changeDetectorRef.markForCheck();
     });
   }
   UpdateExtrinsicCategory() {
+    let cat;
+    if (this.ngExtrinsic == 'Other') {
+      cat = this.extrinsicCat;
+    } else {
+      cat = this.ngExtrinsic;
+    }
     this.isUpdateLoading = true;
     let payload = {
-      extrinsicCategory: this.extrinsicCat,
+      extrinsicCategory: cat,
       storeProductID: Number(this.selectedProduct.pk_storeProductID),
       update_extrinsic_category: true
     }
     this._storeService.UpdateExtrinsicCategory(payload).pipe(takeUntil(this._unsubscribeAll)).subscribe(res => {
       this.isUpdateLoading = false;
       this._storeService.snackBar('Extrinsic Category Updated Successfully');
-      this.selectedProduct.extrinsicCategory = this.extrinsicCat;
-      this._storeService._storeProduct.next(this.selectedProduct);
+      this.selectedProduct.extrinsicCategory = cat;
       this._changeDetectorRef.markForCheck();
     }, err => {
       this.isUpdateLoading = false;
