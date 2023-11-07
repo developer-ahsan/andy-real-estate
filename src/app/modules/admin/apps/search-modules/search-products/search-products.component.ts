1b8eccf83df123fd10eed2ec3ef7483640941612
developer-ahsan <aayoub@cinnova.com>
2023-11-07 18:42:16 +0500

    search product

--- src/app/modules/admin/apps/search-modules/search-products/search-products.component.ts
+++ src/app/modules/admin/apps/search-modules/search-products/search-products.component.ts
@@ -65,7 +65,44 @@ export class SearchProductssComponents implements OnInit, OnDestroy {
             this._changeDetectorRef.markForCheck();
             this.getProductsData(1);
         });
+        // Subscribe to MatDrawer opened change
+        this.matDrawer.openedChange.subscribe((opened) => {
+            if (!opened) {
+                // Remove the selected contact when drawer closed
 
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            }
+        });
+
+        // Subscribe to media changes
+        this._fuseMediaWatcherService.onMediaChange$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(({ matchingAliases }) => {
+
+                // Set the drawerMode if the given breakpoint is active
+                if (matchingAliases.includes('lg')) {
+                    this.drawerMode = 'side';
+                }
+                else {
+                    this.drawerMode = 'over';
+                }
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
+
+        // Listen for shortcuts
+        fromEvent(this._document, 'keydown')
+            .pipe(
+                takeUntil(this._unsubscribeAll),
+                filter<KeyboardEvent>(event =>
+                    (event.ctrlKey === true || event.metaKey) // Ctrl or Cmd
+                    && (event.key === '/') // '/'
+                )
+            )
+            .subscribe(() => {
+            });
     }
     searchProduct() {
         if (this.searchKeyword == '') {
