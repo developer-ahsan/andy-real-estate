27eace6f7c04f0b46b841dfcb200156d8fef4673
developer-ahsan <aayoub@cinnova.com>
2023-11-21 18:41:32 +0500

    core products

--- src/app/modules/admin/apps/ecommerce/inventory/navigation/core-products/core-products.component.ts
+++ src/app/modules/admin/apps/ecommerce/inventory/navigation/core-products/core-products.component.ts
@@ -4,6 +4,7 @@ import { Colors, AvailableCores, SubCategories, Categories } from 'app/modules/a
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-core-products',
@@ -16,7 +17,8 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
 
   displayedColumns: string[] = ['core', 'category', 'sub_category'];
   flashMessage: 'success' | 'error' | null = null;
-  dataSource: Colors[] = [];
+  dataSource = [];
+  coreProducts = [];
   selection;
   selectedorder: string = 'select_order';
   products: string[] = [
@@ -35,6 +37,8 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
   isSubCategory = false;
   coreAddLoader = false;
 
+  isProductLoading: boolean = false;
+
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
     private _inventoryService: InventoryService,
@@ -42,7 +46,6 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
-
     this.getProductDetail();
   }
   getProductDetail() {
@@ -56,16 +59,24 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
   }
   getCoreProducts() {
     const { pk_productID } = this.selectedProduct;
-    this._inventoryService.getCoresByProductId(pk_productID)
+    let params = {
+      core: true,
+      products: true,
+      product_id: pk_productID
+    }
+    this._inventoryService.getProductsData(params)
       .pipe(takeUntil(this._unsubscribeAll))
       .subscribe((cores) => {
         this._inventoryService.getAvailableCoresProductId(pk_productID)
           .pipe(takeUntil(this._unsubscribeAll))
           .subscribe((available_core) => {
             this.available_cores = available_core["data"];
-            this.dataSource = cores["data"];
+            this.coreProducts = cores["data"];
             this.isLoading = false;
-
+            this.selectedCategory = null;
+            this.selectedCore = null;
+            this.dataSource = [];
+            this.selectedSubCategory = null;
             // Mark for check
             this._changeDetectorRef.markForCheck();
           }, err => {
@@ -89,6 +100,9 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
         // Mark for check
         this._changeDetectorRef.markForCheck();
       });
+  }
+  getAvailableCoresProductId() {
+
   }
   uploadImage(): void {
   }
@@ -138,6 +152,8 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
   subCategorySelection(subCategory): void {
     console.log(subCategory)
     this.selectedSubCategory = subCategory;
+    this.isProductLoading = true;
+    this._changeDetectorRef.markForCheck();
     this.getCoreProductsData();
   }
 
@@ -147,11 +163,43 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
       sub_category_id: this.selectedSubCategory.pk_subCategoryID
     }
     this._inventoryService.getProductsData(params).subscribe(res => {
-      console.log(res);
-    })
+      res["data"].forEach(products => {
+        products.imgUrl = environment.assetsURL + `globalAssets/Products/HiRes/${products.storeProductID}.jpg`
+        products.cost = 0;
+        products.imprints = [];
+        if (products.imprintDetails) {
+          const imprints = products.imprintDetails.split(',,');
+          imprints.forEach(imprint => {
+            const [name, price] = imprint.split('::');
+            products.imprints.push({ name, price: Number(price) });
+          });
+        }
+        if (products.costDetails) {
+          const costs = products.costDetails.split('::');
+          const [qty, price] = costs[0].split(',');
+          products.cost = Number(price);
+        }
+      });
+      this.dataSource = res["data"];
+      this.isProductLoading = false;
+      this._changeDetectorRef.markForCheck();
+    });
   }
-
+  cancelCore() {
+    this.selectedCategory = null;
+    this.selectedCore = null;
+    this.selectedSubCategory = null;
+    this.dataSource = [];
+  }
   addCore(): void {
+    if (!this.selectedSubCategory) {
+      this._snackBar.open("Please select a subcategory to add this product to.", '', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 3500
+      });
+      return;
+    }
     const { pk_subCategoryID } = this.selectedSubCategory;
     const { pk_productID } = this.selectedProduct;
     const payload = {
@@ -162,62 +210,26 @@ export class CoreProductsComponent implements OnInit, OnDestroy {
     this.coreAddLoader = true;
     this._inventoryService.addCore(payload)
       .subscribe((response) => {
-        this._inventoryService.getCoresByProductId(pk_productID)
-          .pipe(takeUntil(this._unsubscribeAll))
-          .subscribe((cores) => {
-            this.coreAddLoader = false;
-            this.showFlashMessage(
-              response["success"] === true ?
-                'success' :
-                'error'
-            );
-            this.dataSource = cores["data"];
-            this._snackBar.open("Cores list updated", '', {
-              horizontalPosition: 'center',
-              verticalPosition: 'bottom',
-              duration: 3500
-            });
-
-            this.selectedSubCategory = "";
-            this.selectedCategory = "";
-            this.selectedCore = "";
-
-            // Mark for check
-            this._changeDetectorRef.markForCheck();
-          }, err => {
-            this._snackBar.open("Some error occured", '', {
-              horizontalPosition: 'center',
-              verticalPosition: 'bottom',
-              duration: 3500
-            });
-            this.coreAddLoader = false;
-
-            this.selectedSubCategory = "";
-            this.selectedCategory = "";
-            this.selectedCore = "";
-
-            // Mark for check
-            this._changeDetectorRef.markForCheck();
-          });
-      }, err => {
-        this._snackBar.open("Some error occured", '', {
+        this.getCoreProducts();
+        this.showFlashMessage(
+          response["success"] === true ?
+            'success' :
+            'error'
+        );
+        this._snackBar.open(response["message"], '', {
           horizontalPosition: 'center',
           verticalPosition: 'bottom',
           duration: 3500
         });
         this.coreAddLoader = false;
-
-        this.selectedSubCategory = "";
-        this.selectedCategory = "";
-        this.selectedCore = "";
         // Mark for check
         this._changeDetectorRef.markForCheck();
       });
   }
 
   /**
- * Show flash message
- */
+  * Show flash message
+  */
   showFlashMessage(type: 'success' | 'error'): void {
     // Show the message
     this.flashMessage = type;
