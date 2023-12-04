be11dbb40506fe2e0ea886ad850b346bc8bb4cad
Ahmad Aijaz <ahmadaijaz@Ahmads-MacBook-Pro.local>
2023-12-04 16:20:40 +0500

    fixed issues

--- src/app/modules/admin/apps/vendors/components/pages/open-purchase-orders/open-purchase-orders.component.ts
+++ src/app/modules/admin/apps/vendors/components/pages/open-purchase-orders/open-purchase-orders.component.ts
@@ -31,61 +31,64 @@ export class OpenPurchaseOrdersComponent implements OnInit, OnDestroy {
       key: ['fk_orderID', 'pk_orderLineID'],
       width: '15',
       moneyField: false,
-      isArray: true
+      isArray: true,
+      sortName: 'PO',
+      showIcon: false
     },
     {
       value: 'STATUS',
       key: 'statusName',
       width: '20',
       moneyField: false,
-      isArray: false
+      isArray: false,
+      sortName: 'status',
+      showIcon: false
     },
     {
       value: 'EST. SHIP DATE',
       key: 'estimatedShippingDate',
-      width: '15',
+      width: '20',
       moneyField: false,
-      isArray: false
+      isArray: false,
+      sortName: 'estShipDate',
+      showIcon: false
     },
     {
       value: 'STORE',
       key: 'storeName',
       width: '10',
       moneyField: false,
-      isArray: false
+      isArray: false,
+      sortName: 'store',
+      showIcon: false
     },
     {
       value: 'CUSTOMER',
       key: ['companyName', 'firstName', 'lastName'],
-      width: '30',
+      width: '25',
       moneyField: false,
-      isArray: true
+      isArray: true,
+      sortName: 'customer',
+      showIcon: false
     },
     {
       value: 'TOTAL',
       key: 'TOTAL',
       width: '10',
       moneyField: true,
-      isArray: false
-
+      isArray: false,
+      sortName: 'total',
+      showIcon: false
     }
   ];
   totalRecords = 20;
   page = 1;
   userData: any;
-
-  // params: any = {
-  //   time_frame: 'all',
-  //   status_id: 999,
-  //   admin_user_id: 0,
-  //   tickets_list: true,
-  //   page: this.page,
-  //   keyword: '',
-  // }
-
-
   params: any = {}
   vendorData: any;
+
+  sortingOrder: any = 'ASC'
+  sortBy: any = '';
 
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
@@ -126,8 +129,8 @@ export class OpenPurchaseOrdersComponent implements OnInit, OnDestroy {
       store_id: this.selectedStoreId,
       order_search: this.searchOrder,
       customer_search: this.searchCustomer,
-      sort_order: '',
-      sort_by: '',
+      sort_order: this.sortingOrder,
+      sort_by: this.sortBy,
       page: this.page,
     }
     this.isLoading = true;
@@ -160,11 +163,33 @@ export class OpenPurchaseOrdersComponent implements OnInit, OnDestroy {
     this.getData();
   }
 
+  setSort(column: any) {
+    this.sortBy = column.sortName;
+    if (this.sortingOrder === 'ASC') {
+      this.sortingOrder = 'DESC'
+    } else {
+      this.sortingOrder = 'ASC'
+    }
+    this.displayedColumns.forEach((item) => {
+      if (item.sortName === column.sortName) {
+        item.showIcon = true;
+      } else {
+        item.showIcon = false;
+      }
+    })
+    this.getData();
+  }
+
   resetParams() {
     this.searchOrder = '';
     this.searchCustomer = '';
     this.selectedStoreId = 0;
     this.selectedStore = 'All';
+    this.sortingOrder = 'ASC'
+    this.sortBy = '';
+    this.displayedColumns.forEach((item) => {
+      item.showIcon = false;
+    })
     this.getData();
   }
 
