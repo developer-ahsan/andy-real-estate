a73761a831fc0603f6d599b689cd8540cced5710
developer-ahsan <aayoub@cinnova.com>
2024-02-19 11:41:11 +0500

    order manage

--- src/app/modules/admin/orderManage/components/order-manage.component.ts
+++ src/app/modules/admin/orderManage/components/order-manage.component.ts
@@ -129,7 +129,6 @@ export class OrderManageComponent {
         }
     }
     filterOrderManageData() {
-        this.status = 0;
         const queryParams: NavigationExtras = {
             queryParams: {}
         };
